03b11ca108f6751f256f49aa7ae384a07df1bc2c
Caesar <caioplays@outlook.com>
2023-07-12 16:52:46 -0300

    ADD: Camera movement

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    
+    void Update()
+    {
+        transform.position =  new Vector3(
+            target.transform.position.x,
+            target.transform.position.y, 
+            transform.position.z);
+    }
+
+}
