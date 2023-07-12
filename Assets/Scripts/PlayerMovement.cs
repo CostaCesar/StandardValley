d694ae11a8643ffcf6b3a9d1e439edc8075db340
Caesar <caioplays@outlook.com>
2023-07-12 13:52:39 -0300

    ADD: Player movement & animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+    private Rigidbody2D playerBody;
+    private Animator playerAnimator;
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        playerBody.velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ) * playerSpeed * Time.deltaTime;
+
+        playerAnimator.SetFloat("inputHorizontal", playerBody.velocity.x);
+        playerAnimator.SetFloat("inputVertical", playerBody.velocity.y);
+    }
+}
