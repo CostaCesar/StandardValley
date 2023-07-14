9ad67838e0277387a9cda8b49021e7cd0af6c118
CostaCesar <caioplays@outlook.com>
2023-07-14 14:47:25 -0300

    ADD: Camera smoothing

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public Transform target;
+    [SerializeField] private float cameraSmooth;
     
     void LateUpdate()
     {
-        transform.position =  new Vector3(
-            target.transform.position.x,
-            target.transform.position.y, 
-            transform.position.z);
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new  Vector3(
+                target.transform.position.x,
+                target.transform.position.y, 
+                transform.position.z);
+            
+            // Se cameraSmooth > 0, mover suavemente a camera
+            if(cameraSmooth > 0)
+                transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSmooth);
+            else transform.position = targetPosition;
+        }
     }
 
 }
